91d2d4823144af71d8a6a4ef4b8b4891ed0bc350
Eugene Clark <clarkjr2016@gmail.com>
2022-11-30 18:29:38 -0500

    Removed previous functions to set up refactoring

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 
 function add(...args) {
-    secondSavedNumValue = args[1];
     const array = Array.from(args);
    return array.reduce((total, newNum) => total + newNum, 0);
 }; //function for addition
 
 function subtract(...args) {
-    secondSavedNumValue = args[1]; // this is used as a separate value later in the function.
+
     const array = Array.from(args);
     return array.reduce((total, newNum) => total - newNum);
 }; //function for subtract
 
 function multiply(...args) {
-    secondSavedNumValue = args[1];
+    
     const array = Array.from(args);
     return array.reduce((total, newNum) => total * newNum)
    
    };// function for multiplication
 
    function divide(...args) {
-    secondSavedNumValue = args[1];
     const array = Array.from(args);
     return array.reduce((total, newNum) => total / newNum)
    
@@ -27,26 +25,7 @@ function multiply(...args) {
 
 function operate() {
 
-    secondSavedNum = firstInput.innerText;
-    const firstNum = savedNum;
-    const secondNum = secondSavedNum;
-
-    /*  Continue from here, figure out where to store these values in ordered to be displayed and manipulated */
-    const operands = ["*","+","-","/"];
-    const operandPrompt = operandSelection;
-    displayText.innerText = `${savedNum} ${operandSelection} ${secondSavedNumValue} = `
-
-    if (operandPrompt  == operands[0]) {
-        return multiply(firstNum, secondNum)
-    } else if (operandPrompt == operands[1]) {
-        return add(firstNum, secondNum)
-    } else if (operandPrompt == operands[2]) {
-        return subtract(firstNum, secondNum);
-} else if (operandPrompt == operands[3]) {
-    return divide(firstNum, secondNum);
-};
-
-
+  
 
 }
 
@@ -68,17 +47,9 @@ const seven = document.querySelector("#seven"); //variable for seven button
 const eight = document.querySelector("#eight")
 const nine = document.querySelector("#nine")
 const firstInput = document.querySelector("#first-input"); // variable for display text where answers are to be displayed
-
 const division = document.querySelector("#division");
 const equal = document.querySelector("#equal")
 const displayText  = document.querySelector("#display-text");
-
-
-equal.addEventListener("click", () => {
-const result = operate();
-
-firstInput.innerText = result;
-})
 
 
 clear.addEventListener("click", () => {
@@ -106,38 +77,4 @@ division.addEventListener("click", (e) => {
     //console.log(result);
 
 })
-
-equal.addEventListener("click", operate);
-
-zero.addEventListener("click", (e) =>
-firstInput.innerText += e.target.innerText); // adds the inne text from the associated 
-
-one.addEventListener("click", (e) =>
-firstInput.innerText += e.target.innerText);
-
-two.addEventListener("click", (e) =>
-firstInput.innerText += e.target.innerText);
-
-three.addEventListener("click", (e) =>
-firstInput.innerText += e.target.innerText);
-
-four.addEventListener("click", (e) =>
-firstInput.innerText += e.target.innerText);
-
-five.addEventListener("click", (e) =>
-firstInput.innerText += e.target.innerText);
-
-six.addEventListener("click", (e) =>
-firstInput.innerText += e.target.innerText);
-
-seven.addEventListener("click", (e) =>
-firstInput.innerText += e.target.innerText);
-
-eight.addEventListener("click", (e) =>
-firstInput.innerText += e.target.innerText);
-
-
-nine.addEventListener("click", (e) =>
-firstInput.innerText += e.target.innerText);
-
 
